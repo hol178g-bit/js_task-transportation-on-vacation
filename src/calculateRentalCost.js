57f8f0d09ad71cf36ab5611a92e78acafe522ae4
- <a-@MacBook-Air.local>
2026-09-04 21:23:06 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let totalAmount = days * 40;
+
+  if (days >= 3) {
+    totalAmount = days * 40 - 20;
+  }
+
+  if (days >= 7) {
+    totalAmount = days * 40 - 50;
+  }
+
+  return totalAmount;
 }
 
 module.exports = calculateRentalCost;
