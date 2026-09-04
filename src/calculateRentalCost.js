4f9c2d4a50e5752c1e691f74240914f77709f3b8
- <a-@MacBook-Air.local>
2026-09-04 21:37:29 +0300

    reconstruction

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,14 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let totalAmount = days * 40;
+  const DAILY_PRICE = 40;
+  const MEDIUM_TERM = 3;
+  const LONG_TERM = 7;
+  const MEDIUM_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+  let totalAmount = days * DAILY_PRICE;
 
-  if (days >= 3) {
-    totalAmount = days * 40 - 20;
+  if (days >= MEDIUM_TERM) {
+    totalAmount = days * DAILY_PRICE - MEDIUM_TERM_DISCOUNT;
   }
 
-  if (days >= 7) {
-    totalAmount = days * 40 - 50;
+  if (days >= LONG_TERM) {
+    totalAmount = days * DAILY_PRICE - LONG_TERM_DISCOUNT;
   }
 
   return totalAmount;
